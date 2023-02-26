8d5c016c823cc344067d6a5b5ead5a057941bc6e
Gamddalki <hannahk01@naver.com>
2023-02-26 13:36:18 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#ffffff",
+  textColor: "#2f3640",
+  accentColor: "#B47F49",
+  boardColor: "#f1f2f6",
+  cardColor: "#ffffff",
+  headerColor: "#111",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#e84118",
+  boardColor: "#f1f2f6",
+  cardColor: "#2d3436",
+  headerColor: "#111",
+};

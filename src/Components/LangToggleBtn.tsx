f8627dda138893590f9d7a94463509ee82dec1ac
Gamddalki <hannahk01@naver.com>
2023-02-28 01:23:14 +0900

    Create LangToggleBtn.tsx

---
+++ src/Components/LangToggleBtn.tsx
@@ -0,0 +1,40 @@
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
+import { isEngAtom } from "../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+const ToggleBtn = styled.button`
+  margin-right: 20px;
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 12px;
+`;
+
+function LangToggleBtn() {
+  const { t, i18n } = useTranslation("header");
+
+  const isEng = useRecoilValue(isEngAtom);
+  const setEngAtom = useSetRecoilState(isEngAtom);
+  const toggleEngAtom = () => {
+    toggleLanguage();
+    setEngAtom((prev) => !prev);
+    console.log(isEng);
+  };
+
+  const toggleLanguage = () => {
+    var locale = isEng ? "co-KR" : "en-US";
+    i18n.changeLanguage(locale);
+  };
+
+  return (
+    <ToggleBtn onClick={() => toggleEngAtom()}>
+      <FontAwesomeIcon icon={faEarthAmericas} size="1x" />
+      <span> {t("lang")}</span>
+    </ToggleBtn>
+  );
+}
+
+export default LangToggleBtn;

22cb4ca2e45732225d617d5451cab66e4628a7f2
Gamddalki <hannahk01@naver.com>
2023-02-28 12:41:27 +0900

    Add iframes to Home.tsx

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -14,11 +14,65 @@ const Background = styled.div`
   }
 `;
 
+const IframeBox = styled.div`
+  width: 100%;
+  height: 14vw;
+  display: flex;
+  background-color: black;
+  div {
+    flex: 1;
+  }
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Iframe = ({ src }: { src: string }) => {
+  return (
+    <div>
+      <iframe
+        src={src}
+        frameBorder={0}
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
+    </div>
+  );
+};
+
+const IframeContainer = ({
+  src1,
+  src2,
+  src3,
+}: {
+  src1: string;
+  src2: string;
+  src3: string;
+}) => {
+  return (
+    <IframeBox>
+      <Iframe src={src1} />
+      <Iframe src={src2} />
+      <Iframe src={src3} />
+      <Iframe src={src3} />
+    </IframeBox>
+  );
 function Home() {
   return (
     <Background>
       <img src={bg2} />
     </Background>
+            style={{ color: "rgba(235,235,235,0.3)" }}
+          />
+        </SlideBtn>
+      </Background>
+      <IframeContainer
+        src1="https://www.youtube.com/embed/WUsjSMXHJXE?autoplay=1&mute=1&loop=1;"
+        src2="https://www.youtube.com/embed/ZUnHFFSTPOY?autoplay=1&mute=1&loop=1;"
+        src3="https://www.youtube.com/embed/PSPzam3fIrk?autoplay=1&mute=1&loop=1;"
+      />
+    </>
   );
 }
 

583abe9ff168bca99848ea010b3ef82569ed33b8
Gamddalki <hannahk01@naver.com>
2023-03-03 23:59:43 +0900

    Update theme.ts
    Remove Night Mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,21 +1,11 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "#ffffff",
-  textColor: "#2f3640",
+  bgColor: "#2F2F2F",
+  textColor: "white",
   accentColor: "#FCE49C",
   boardColor: "#f1f2f6",
   cardColor: "#ffffff",
   headerColor: "rgba(0, 0, 0, 0.5)",
-  headertextColor: "white",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "#e84118",
-  boardColor: "#f1f2f6",
-  cardColor: "#2d3436",
-  headerColor: "#111",
   headertextColor: "white",
 };

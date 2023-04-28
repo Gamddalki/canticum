475d6d0ff106915f21e81211ca8774373813784a
Gamddalki <hannahk01@naver.com>
2023-04-29 02:28:15 +0900

    Update index.ts

--- src/locales/ko/index.ts
+++ src/locales/ko/index.ts
@@ -1,5 +1,6 @@
 import header from "./header.json";
 import about from "./about.json";
 import recruitment from "./recruitment.json";
+import concert from "./concert.json";
 
-export { header, about, recruitment };
+export { header, about, recruitment, concert };

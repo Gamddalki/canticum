22c897c53a7727928f378890c3d6a15a49ec1467
Gamddalki <hannahk01@naver.com>
2024-02-05 18:01:02 +0900

    Update Concert Cards

--- src/Routes/news/Concert.tsx
+++ src/Routes/news/Concert.tsx
@@ -2,9 +2,15 @@ import BodyWrapper from "../../Components/BodyWrapper";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import conimg1 from "../../img/concert/230530_1.jpg";
+import conimg2 from "../../img/concert/231214_1.jpg";
 import ConcertCard from "../../Components/ConcertCard";
 
-const ConcertBoard = styled.div``;
+const ConcertBoard = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
 
 function Concert() {
   const { t, i18n } = useTranslation("concert");
@@ -17,10 +23,16 @@ function Concert() {
       >
         <ConcertBoard>
           <ConcertCard
+            linkto="/newsletter/concert/231214"
+            imgsrc={conimg2}
+            contitle={t("contitle2")}
+            conwhen={t("conwhen2")}
+          ></ConcertCard>
+          <ConcertCard
             linkto="/newsletter/concert/230530"
             imgsrc={conimg1}
-            contitle={t("contitle")}
-            conwhen={t("conwhen")}
+            contitle={t("contitle1")}
+            conwhen={t("conwhen1")}
           ></ConcertCard>
         </ConcertBoard>
       </BodyWrapper>

09784e7aa4267e1183083bc0b7dfd553fc85b023
Gamddalki <hannahk01@naver.com>
2024-06-05 12:39:24 +0900

    이미지 경로 DB에서 가져오게 수정

--- src/Routes/news/Newsletter.tsx
+++ src/Routes/news/Newsletter.tsx
@@ -1,13 +1,22 @@
 import BodyWrapper from "../../Components/BodyWrapper";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import newsimg1 from "../../img/news/202305_01.jpg";
 import ConcertCard from "../../Components/ConcertCard";
+import useImages from "../../hooks/useImages";
 
-const ConcertBoard = styled.div``;
+const ConcertBoard = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
 
 function Newsletter() {
   const { t, i18n } = useTranslation("newsletter");
+  const { images, error } = useImages({ type: "news" });
+
+  const coverImages = images.filter((image) => image.filename.includes("_01"));
+
   return (
     <>
       <BodyWrapper
@@ -16,12 +25,19 @@ function Newsletter() {
         pageTxt={t("pagetxt")}
       >
         <ConcertBoard>
-          <ConcertCard
-            linkto="/newsletter/2305"
-            imgsrc={newsimg1}
-            contitle={t("newstitle")}
-            conwhen={t("newswhen")}
-          ></ConcertCard>
+          {error ? (
+            <div>{t("error_loading_images")}</div> // 이미지 로드 오류 메시지
+          ) : (
+            coverImages.map((image, index) => (
+              <ConcertCard
+                key={index}
+                linkto={`/newsletter/${image.filename.split("_")[0]}`} // 링크 생성
+                imgsrc={image.filepath}
+                contitle={t(`newstitle${image.filename.split("_")[0]}`)}
+                conwhen={t(`newswhen${image.filename.split("_")[0]}`)}
+              ></ConcertCard>
+            ))
+          )}
         </ConcertBoard>
       </BodyWrapper>
     </>
--- src/Routes/news/NewsletterDetail.tsx
+++ src/Routes/news/NewsletterDetail.tsx
@@ -1,11 +1,8 @@
 import BodyWrapper from "../../Components/BodyWrapper";
+import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import newsimg1 from "../../img/news/202305_01.jpg";
-import newsimg2 from "../../img/news/202305_02.jpg";
-import newsimg3 from "../../img/news/202305_03.jpg";
-import newsimg4 from "../../img/news/202305_04.jpg";
-import newsimg5 from "../../img/news/202305_05.jpg";
+import useImages from "../../hooks/useImages";
 
 const ImgWrapper = styled.div`
   width: 100%;
@@ -17,19 +14,34 @@ const ImgWrapper = styled.div`
 
 function NewsletterDetail() {
   const { t, i18n } = useTranslation("newsletter");
+  const { id } = useParams();
+  console.log(id);
+  const { images, error } = useImages({
+    type: "news",
+    id: id ? id : "",
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!images) {
+    return <div>Loading...</div>;
+  }
+
+  const newsTitle = `newstitle${id}`;
+
   return (
     <>
       <BodyWrapper
-        pageTitle={t("newstitle")}
+        pageTitle={t(newsTitle)}
         pageSubtitle={t("pagetxt")}
         pageTxt={t("pagetxt")}
       >
         <ImgWrapper>
-          <img src={newsimg1}></img>
-          <img src={newsimg2}></img>
-          <img src={newsimg3}></img>
-          <img src={newsimg4}></img>
-          <img src={newsimg5}></img>
+          {images.map((image, index) => (
+            <img key={index} src={image.filepath}></img>
+          ))}
         </ImgWrapper>
       </BodyWrapper>
     </>

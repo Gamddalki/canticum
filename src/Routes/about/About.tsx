bcc1ba7eebef1e0bbc7319536ffa319cf93c3ce2
Gamddalki <hannahk01@naver.com>
2024-06-05 12:39:31 +0900

    Update About.tsx

--- src/Routes/about/About.tsx
+++ src/Routes/about/About.tsx
@@ -48,7 +48,7 @@ function About() {
           >
             <IFrameWrapper>
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.4021384946805!2d126.81563051212673!3d37.56914727192128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9c8f62a18c45%3A0x1256e4f335f96a1!2z7KO87Zal7ZWc6rWQ7ZqM!5e0!3m2!1sen!2skr!4v1682568484254!5m2!1sen!2skr"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12651.393634431266!2d126.93683342581339!3d37.558635164729736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c989b39e3c087%3A0x71f8f20d4ca79ad2!2z7Iug7ZiE6rWQ7ZqM!5e0!3m2!1sko!2skr!4v1715577927615!5m2!1sko!2skr"
                 width="100%"
                 height="450"
                 frameBorder={0}
